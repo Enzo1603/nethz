aab14b47dd8ab39df963bae9f48554255416de38
Enzo1603 <96661578+Enzo1603@users.noreply.github.com>
2023-12-01 23:30:54 +0100

    Track lib folder

---
+++ lib/templatetags/macros.py
@@ -0,0 +1,37 @@
+from django import template
+from django.templatetags.static import static
+
+register = template.Library()
+
+
+@register.inclusion_tag("macros/card.html")
+def card(card_data):
+    """card_data = {
+    "title": "Title",
+    "description": "Description",
+    "button_text": "Button Text",
+    "image_path": "Image/Path",
+    "link": "Link",
+    "disable": True/False,
+    }
+    """
+    return card_data
+
+
+@register.inclusion_tag("macros/card.html")
+def coming_soon_card():
+    return card(
+        {
+            "title": "Coming soon (or never)",
+            "description": "nothing to be seen here",
+            "button_text": "Go away",
+            "image_path": static("images/informatik1_3px.jpg"),
+            "link": "#",
+            "disable": True,
+        }
+    )
+
+
+@register.inclusion_tag("macros/regions.html")
+def region_select(region):
+    return {"region": region}
