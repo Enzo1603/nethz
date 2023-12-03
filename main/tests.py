caca21909069dca4c73d4d26acb9a56995bb3960
Enzo <96661578+Enzo1603@users.noreply.github.com>
2023-12-03 23:01:17 +0100

    Initial tests

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,66 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.templatetags.static import static
+
 
 # Create your tests here.
+class MainViewsTest(TestCase):
+    def test_home_view(self):
+        response = self.client.get(reverse("main:home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "main/home.html")
+
+        # Add more assertions for the content of the response, if needed
+        self.assertContains(response, "Technische Mechanik 2023")
+        self.assertContains(response, "Physik I 2024")
+        self.assertContains(response, "Informatik I 2024")
+        self.assertContains(response, "Worldle")
+
+    def test_technische_mechanik_view(self):
+        response_valid = self.client.get(
+            reverse("main:technische_mechanik", args=["HS23"])
+        )
+        self.assertEqual(response_valid.status_code, 200)
+        self.assertTemplateUsed(response_valid, "technische_mechanik/TM_HS23.html")
+
+        response_invalid = self.client.get(
+            reverse("main:technische_mechanik", args=["InvalidSemester"])
+        )
+        self.assertEqual(response_invalid.status_code, 404)
+
+    def test_home_view_card_links(self):
+        response = self.client.get(reverse("main:home"))
+        self.assertEqual(response.status_code, 200)
+
+        # Test that the links in the cards are correct
+        self.assertContains(
+            response,
+            'href="' + reverse("main:technische_mechanik", args=["HS23"]) + '"',
+        )
+        self.assertContains(
+            response,
+            'href="#"',  # You may want to update this once you have a valid link for ph_card and inf_card
+        )
+        self.assertContains(response, 'href="' + reverse("worldle:home") + '"')
+
+    def test_home_view_card_images(self):
+        response = self.client.get(reverse("main:home"))
+        self.assertEqual(response.status_code, 200)
+
+        # Test that the image paths in the cards are correct
+        self.assertContains(
+            response,
+            'src="' + static("images/technische_mechanik_6px.jpg") + '"',
+        )
+        self.assertContains(
+            response,
+            'src="' + static("images/physik1_2px.jpg") + '"',
+        )
+        self.assertContains(
+            response,
+            'src="' + static("images/informatik1_3px.jpg") + '"',
+        )
+        self.assertContains(
+            response,
+            'src="' + static("images/Earth_2px.jpg") + '"',
+        )
--- worldle/tests.py
+++ worldle/tests.py
@@ -1,3 +1,87 @@
+from django.http import Http404, JsonResponse
 from django.test import TestCase
+from django.urls import reverse
+
+from worldle.views import DEFAULT_REGION
+
 
 # Create your tests here.
+class WorldleViewsTest(TestCase):
+    def test_home_view(self):
+        response = self.client.get(reverse("worldle:home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "worldle/home.html")
+
+        # Add more assertions for the content of the response, if needed
+        self.assertContains(response, "Capitals")
+        self.assertContains(response, "Languages")
+        self.assertContains(response, "Areas")
+
+    def test_default_capitals_view(self):
+        response = self.client.get(reverse("worldle:default_capitals"))
+        self.assertRedirects(
+            response, reverse("worldle:capitals", args=[DEFAULT_REGION])
+        )
+
+    def test_capitals_view(self):
+        # Test with a valid region
+        response = self.client.get(reverse("worldle:capitals", args=["africa"]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "worldle/capitals.html")
+
+        # Test with an invalid region
+        response_invalid = self.client.get(
+            reverse(
+                "main:technische_mechanik",
+                args=["invalid_region"],
+            ),
+        )
+        self.assertEqual(response_invalid.status_code, 404)
+
+        # Add more test cases as needed
+
+    def test_default_languages_view(self):
+        response = self.client.get(reverse("worldle:default_languages"))
+        self.assertRedirects(
+            response, reverse("worldle:languages", args=[DEFAULT_REGION])
+        )
+
+    def test_languages_view(self):
+        # Test with a valid region
+        response = self.client.get(reverse("worldle:languages", args=["americas"]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "worldle/languages.html")
+
+        # Test with an invalid region
+        response_invalid = self.client.get("worldle:languages", args=["invalid_region"])
+        self.assertEqual(response_invalid.status_code, 404)
+
+        # Add more test cases as needed
+
+    def test_areas_view(self):
+        response = self.client.get(reverse("worldle:areas"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "worldle/areas.html")
+
+        # Add more assertions for the content of the response, if needed
+
+    def test_get_country_view(self):
+        response = self.client.get(reverse("worldle:get_country"))
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response, JsonResponse)
+
+        # Add more assertions for the content of the response, if needed
+        json_data = response.json()
+        self.assertIn("country", json_data)
+        country = json_data["country"]
+        self.assertIsInstance(country, dict)
+
+        # Ensure that the country has the expected fields
+        expected_fields = {
+            "name.common",
+            "cca3",
+            "area",
+        }
+        self.assertTrue(expected_fields.issubset(country.keys()))
+
+    # Add more test cases as needed
