6b831c625ff8afede3680dda7bacff49a7a67989
Enzo1603 <96661578+Enzo1603@users.noreply.github.com>
2023-12-12 21:10:09 +0100

    Add tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,97 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model, authenticate
 
-# Create your tests here.
+from accounts.models import CustomUser
+
+
+class CustomUserTests(TestCase):
+    def setUp(self):
+        self.user_data = {
+            "username": "testuser",
+            "email": "test@example.com",
+            "password": "testpassword@12",
+        }
+        self.user = get_user_model().objects.create_user(**self.user_data)
+
+    def test_user_creation(self):
+        self.assertEqual(self.user.username, self.user_data["username"])
+        self.assertEqual(self.user.email, self.user_data["email"])
+        self.assertTrue(self.user.check_password(self.user_data["password"]))
+
+    def test_signup_view(self):
+        url = reverse("accounts:signup")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        # Test signup functionality
+        response = self.client.post(url, data=self.user_data)
+        self.assertEqual(response.status_code, 200)
+
+        # Check if the user is actually created
+        self.assertTrue(
+            get_user_model().objects.filter(email=self.user_data["email"]).exists()
+        )
+
+    def test_login_view(self):
+        # Log in first
+        login = self.client.login(
+            email=self.user_data["email"], password=self.user_data["password"]
+        )
+        self.assertTrue(login)
+
+        url = reverse("accounts:login")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+        # Test login functionality
+        response = self.client.post(
+            url,
+            data={
+                "email": self.user_data["email"],
+                "password": self.user_data["password"],
+            },
+        )
+        self.assertEqual(
+            response.status_code, 302
+        )  # Expecting a redirect after successful login
+
+        # Check if the user is logged in
+        user = authenticate(
+            email=self.user_data["email"], password=self.user_data["password"]
+        )
+        self.assertTrue(user.is_authenticated)
+
+    def test_logout_view(self):
+        # Log in first
+        self.client.login(
+            email=self.user_data["email"], password=self.user_data["password"]
+        )
+
+        url = reverse("accounts:logout")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)  # Redirects after logout
+
+        # Check if the user is logged out
+        self.assertFalse("_auth_user_id" in self.client.session)
+
+    def test_user_update_view(self):
+        # Log in first
+        login = self.client.login(
+            email=self.user_data["email"], password=self.user_data["password"]
+        )
+        self.assertTrue(login)
+
+        url = reverse("accounts:user_account")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        # Test user update functionality
+        updated_data = {"username": "newusername", "email": "newemail@example.com"}
+        response = self.client.post(url, data=updated_data, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        # Check if the user is actually updated
+        user = CustomUser.objects.get(username=updated_data["username"])
+        self.assertEqual(user.username, updated_data["username"])
+        self.assertEqual(user.email, updated_data["email"])
--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ app_name = "accounts"
 
 
 urlpatterns = [
-    path("signup/", views.SignUpView.as_view(), name="sign_up"),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
     path("login/", views.CustomLoginView.as_view(), name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("user_account/", views.UserUpdateView.as_view(), name="user_account"),
