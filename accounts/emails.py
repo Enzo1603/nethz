4216c4b38490a19523bdd5b17b8f0386b422139c
Enzo <96661578+Enzo1603@users.noreply.github.com>
2024-04-13 23:29:15 +0200

    Remove threaded emails since server seems to not execute the sending unless restarted

--- accounts/emails.py
+++ accounts/emails.py
@@ -1,4 +1,4 @@
-from threading import Thread
+# from threading import Thread
 
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
@@ -10,36 +10,36 @@ from django.conf import settings
 
 
 def send_verification_email(user, request):
-    def _send_email():
-        token = default_token_generator.make_token(user)
-        uid = urlsafe_base64_encode(force_bytes(user.pk))
-        current_site = get_current_site(request)
+    # def _send_email():
+    token = default_token_generator.make_token(user)
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    current_site = get_current_site(request)
 
-        mail_subject = "Activate your account"
-        txt_message = render_to_string(
-            "accounts/verification_email.txt",
-            {
-                "user": user,
-                "domain": current_site.domain,
-                "uid": uid,
-                "token": token,
-            },
-        )
-        html_message = render_to_string(
-            "accounts/verification_email.html",
-            {
-                "user": user,
-                "domain": current_site.domain,
-                "uid": uid,
-                "token": token,
-            },
-        )
-        send_mail(
-            mail_subject,
-            txt_message,
-            settings.DEFAULT_FROM_EMAIL,
-            [user.email],
-            html_message=html_message,
-        )
+    mail_subject = "Activate your account"
+    txt_message = render_to_string(
+        "accounts/verification_email.txt",
+        {
+            "user": user,
+            "domain": current_site.domain,
+            "uid": uid,
+            "token": token,
+        },
+    )
+    html_message = render_to_string(
+        "accounts/verification_email.html",
+        {
+            "user": user,
+            "domain": current_site.domain,
+            "uid": uid,
+            "token": token,
+        },
+    )
+    send_mail(
+        mail_subject,
+        txt_message,
+        settings.DEFAULT_FROM_EMAIL,
+        [user.email],
+        html_message=html_message,
+    )
 
-    Thread(target=_send_email).start()
+    # Thread(target=_send_email).start()
