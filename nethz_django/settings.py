0ca2aeac4d9b2f5d023160c20d88a2c7c433a97c
Enzo1603 <96661578+Enzo1603@users.noreply.github.com>
2025-08-12 20:31:57 +0200

    hotfix

--- nethz_django/settings.py
+++ nethz_django/settings.py
@@ -90,7 +90,6 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "lib.middleware.GermanDefaultRedirectMiddleware",
     "django.middleware.locale.LocaleMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
