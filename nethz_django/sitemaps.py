37f217e8d0a9e5d0d1cd9f95751ba2a82a382c66
Enzo1603 <96661578+Enzo1603@users.noreply.github.com>
2025-09-14 19:19:00 +0200

    convert to webp

--- nethz_django/sitemaps.py
+++ nethz_django/sitemaps.py
@@ -19,21 +19,11 @@ class StaticViewSitemap(Sitemap):
             "main:technische_mechanik",
             "worldle:home",
             "worldle:default_capitals",
-<<<<<<< HEAD
-<<<<<<< HEAD
-            "worldle:default_languages",
-=======
-=======
->>>>>>> seo
             "worldle:competitive_capitals",
             "worldle:default_languages",
             "worldle:competitive_languages",
             "worldle:competitive_currencies",
             "worldle:competitive_areas",
-<<<<<<< HEAD
->>>>>>> seo
-=======
->>>>>>> seo
             "worldle:leaderboards",
         ]
 
@@ -61,15 +51,7 @@ class TechnischeMechanikSitemap(Sitemap):
         ).values_list("short_name", flat=True)
 
         # Extract semester part from "TM_HS24" -> "HS24"
-<<<<<<< HEAD
-<<<<<<< HEAD
-        semesters = [session.replace("TM_", "") for session in tm_sessions]
-=======
         semesters = [session.replace("TM_", "").lower() for session in tm_sessions]
->>>>>>> seo
-=======
-        semesters = [session.replace("TM_", "").lower() for session in tm_sessions]
->>>>>>> seo
         return semesters
 
     def location(self, item):
